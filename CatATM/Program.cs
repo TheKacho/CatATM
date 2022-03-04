d753dbe363337e3e97944a14ea5fa8d60d43e576
TheKacho <abyssknightzero@gmail.com>
2022-03-03 20:09:26 -0500

    Add project files.

---
+++ CatATM/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace CatATM
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Cat ATM";
+
+            Console.WriteLine("Hello human! First, please enter your 4 digit Cat PIN.");
+            
+            
+
+            Console.ReadKey();
+        }
+
+        private static string RequestPIN()
+        {
+            StringBuilder sb = new StringBuilder();
+            ConsoleKeyInfo keyInfo;
+
+            do
+            {
+                keyInfo = Console.ReadKey(true);
+
+                if (!char.IsControl(keyInfo.KeyChar)
+                {
+                    sb.Append(keyInfo.KeyChar);
+                    Console.Write("*")
+                }
+            } while (true);
+        }
+    }
+}
