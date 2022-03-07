4830ee5e1a4ad61365bd5fe03d1586a239a28c80
TheKacho <abyssknightzero@gmail.com>
2022-03-07 13:53:10 -0500

    added a catvalidate class

---
+++ CatATM/UI/CatValidate.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CatATM.UI
+{
+    public static class CatValidate
+    {
+        public static T Convert<T>(string prompt)
+        {
+            bool valid = false;
+            string userInput;
+
+            while (!valid)
+            {
+                userInput = Utilities.UserInput(prompt);
+
+                try
+                {
+                    var convert = TypeDescriptor.GetConverter(typeof(T));
+                    if(convert != null)
+                    {
+                        return (T)convert.ConvertFromString(userInput);
+                    }
+                    else
+                    {
+                        return default;
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("The cat hisses angrily at you! \nIts trying to say invalid input, try again!\n");
+                }
+            }
+            return default;
+        }
+    }
+}
